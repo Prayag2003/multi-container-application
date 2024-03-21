e6eef55de213970efcc5995a01aeb59f37c928aa
Prayag Bhatt <prayagbhatt2003@gmail.com>
2024-03-21 09:29:37 +0000

    added models and dockerised the application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import mongoose from "mongoose"
 import * as dotenv from "dotenv"
 dotenv.config()
+import mongoose from "mongoose"
+import express from "express"
+const app = express()
+const port = 3000
 import { DB_NAME } from "./constants.js"
+import { User } from "../models/user.model.js"
 
 const database = `${process.env.MONGO_DB_URI}/${DB_NAME}`
 
@@ -12,3 +16,27 @@ mongoose.connect(database)
     .catch(() => {
         console.log("Error in DB connection");
     })
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+app.post("/user", async (req, res) => {
+    console.log("All the users");
+    const newUser = new User({
+        name: "Prayag",
+        email: "abc123@gmail.com"
+    })
+    await newUser.save()
+        .then(() => {
+            console.log("User created");
+            res.send(newUser)
+        })
+        .catch(() => {
+            console.log("Failed to create user");
+        })
+})
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
